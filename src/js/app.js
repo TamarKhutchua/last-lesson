14f6717790d1c287db7623b175b2fccb92360f5c
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-05-04 23:22:49 +0400

    finito

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,30 @@
-// Get a reference to the table element
-let table = document.getElementById("highlighter2");
-
-// Add an event listener to the table that listens for clicks on the delete buttons
-table.addEventListener("click", function (event) {
-  if (event.target.tagName === "BUTTON") {
-    // Check if the clicked element is a button
-    // Get the row that contains the clicked button
-    let button = event.target;
-    let row = button.parentNode.parentNode; // Assumes button is in a cell in a row
-
-    // Delete the row from the table
-    table.deleteRow(row.rowIndex);
-
-    // Send a delete request to the server
-    let id = row.getAttribute("data-id"); // Assumes the row has a data-id attribute with the ID of the record to delete
-    fetch("/records/" + id, {
-      method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+function clearRow(button) {
+  let row = button.parentNode.parentNode;
+  let cells = row.getElementsByTagName("td");
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].textContent = "";
   }
-});
+}
+
+let url = "https://borjomi.loremipsum.ge/api/delete_row";
+let xhr = new XMLHttpRequest();
+xhr.open("DELETE", url);
+xhr.send();
+
+function updateRow(button) {
+  let row = button.parentNode.parentNode;
+
+  let id = prompt("Enter id:");
+  let name = prompt("Enter name:");
+  let email = prompt("Enter email:");
+
+  let cells = row.getElementsByTagName("td");
+  cells[0].textContent = id;
+  cells[1].textContent = name;
+  cells[2].textContent = email;
+
+  let url = "https://borjomi.loremipsum.ge/api/update_row";
+  let xhr = new XMLHttpRequest();
+  xhr.open("PUT", url);
+  xhr.send(JSON.stringify({ id: id, name: name, email: email }));
+}
